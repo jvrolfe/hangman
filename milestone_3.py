b6553b5098499be37c8442ef65b0436470ed5e52
jvrolfe <jesse.v.rolfe@gmail.com>
2023-10-11 16:57:01 +0100

    Compares guess to chosen word

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,5 @@
 import string
+import milestone_2
 
 while True:
     guess = input("Enter a letter: ")  # User letter input
@@ -6,3 +7,8 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in milestone_2.chosen_word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
