1dcca979e4f4b39ca39c2b31ff9042c76b2934a9
jvrolfe <jesse.v.rolfe@gmail.com>
2023-10-11 16:29:26 +0100

    Refactoring code

--- milestone_2.py
+++ milestone_2.py
@@ -2,17 +2,15 @@ import random   # Import the random module to select a random word from the crea
 import string   # Import the string module to check that a guess is a letter
 
 
-word_list = ["Banana","Apple","Pear","Strawberry","Dragonfruit"]    # a list of words to select from
+hangman_word_list = ["Banana","Apple","Pear","Strawberry","Dragonfruit"]    # A list of words to select from
+print(hangman_word_list)
 
-print(word_list)
+chosen_word = random.choice(hangman_word_list)  # Using the random module with the choice method to randomly choose a word from the list and assining it to the variable word
+print(chosen_word)
 
-word = random.choice(word_list)  # Using the random module with the choice method to randomly choose a word from the list and assining it to the variable word
+user_guess = input("Enter a letter: ")  # User letter input
 
-print(word)
-
-guess = input("Enter a letter: ")  # User letter input
-
-if len(guess) == 1 and guess in string.ascii_letters:
+if len(user_guess) == 1 and user_guess in string.ascii_letters:   # An if statement to check that the user input is a single letter
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
