edc47c4fa8f3a478cf341e72b95b65a777d5331d
jvrolfe <jesse.v.rolfe@gmail.com>
2023-10-13 15:58:13 +0100

    Converting chosen word to lowercase

--- milestone_3.py
+++ milestone_3.py
@@ -2,15 +2,15 @@ import string
 import milestone_2
 
 
-def check_guess(guess):
+def check_guess(guess):     # A function to check if the guessed letteris the computer chosen word
     guess = guess.lower()
-    if guess in milestone_2.chosen_word:
+    if guess in milestone_2.chosen_word.lower():
         print(f"Good guess! {guess} is in the word.")
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
 def ask_for_input():
-    while True:
+    while True:  
         guess = input("Enter a letter: ")  # User letter input
         if len(guess) == 1 and guess.isalpha() == True:
             break
