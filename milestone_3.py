6fcc79da3681d8c174e56837109e18476abe02fd
jvrolfe <jesse.v.rolfe@gmail.com>
2023-10-13 15:34:49 +0100

    milestrone 3 update

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,21 @@
 import string
 import milestone_2
 
-while True:
-    guess = input("Enter a letter: ")  # User letter input
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in milestone_2.chosen_word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in milestone_2.chosen_word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a letter: ")  # User letter input
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
